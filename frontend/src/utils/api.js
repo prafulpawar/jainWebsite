f64ffd2f0978d8156dfd53f9e38a9eadb6bc3cc0
prafull dukhi <prafuldukhi@gmail.com>
2026-02-09 21:18:01 +0530

    Updates

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://jainwebsitebj.onrender.com/api', // Your Backend URL
-   baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://jainwebsitebj.onrender.com/api', // Your Backend URL
+  //  baseURL: 'http://localhost:5000/api'
 });
 
 // Add a request interceptor to include token
